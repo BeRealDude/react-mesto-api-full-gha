7654cb7745bb5c846a40488580e735a4601aaaf2
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-05-29 08:27:58 +0300

    feat: crash test

--- backend/app.js
+++ backend/app.js
@@ -36,6 +36,12 @@ app.use(requestLogger);
 
 app.use(limiter);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', routerSignup);
 app.use('/', routerSignin);
 
